b968e7be50595cf1ca8c547affeaf81209f45c60
nivishree <nivipalg@gmail.com>
2019-09-16 14:58:25 +0530

    Add files via upload
    test proj

---
+++ PatientMonitorChatBotTest/ChatBotRepositoryTest.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using chatbotrepo;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ChatBotControllerTest
+    {
+        [TestMethod]
+        public void GivenMonitoId_WhenItIsValid_ThenGiveCorrectMonitorName()
+        {
+            MonitorRepository m = new MonitorRepository();
+            string Actual = "Avalon CL";
+            string expected = m.DataFetch(1);
+            Assert.AreEqual(Actual, expected);
+        }
+
+        [TestMethod]
+        public void GivenMonitoId_WhenItIsValid_ThenGiveCorrectMonitorName2()
+        {
+            MonitorRepository m = new MonitorRepository();
+            string Actual = "IntelliVue MP5SC";
+            string expected = m.DataFetch(22);
+            Assert.AreEqual(Actual, expected);
+        }
+
+
+        [TestMethod]
+        public void GivenMonitoId_WhenItIsNotValid_ThenShouldNotGiveCorrecttMonitorName()
+        {
+            MonitorRepository m = new MonitorRepository();
+            string Actual = "IntelliVue MP5SC";
+            string expected = m.DataFetch(2);
+            Assert.AreNotEqual(Actual, expected);
+        }
+
+        [TestMethod]
+        public void GivenQuestionId_WhenItIsValid_ThenDisplayCorrectQuestion()
+        {
+            QuestionRepository m = new QuestionRepository();
+            string Actual = "Do you want a monitor with cable or cableless?";
+            string expected = m.DataFetch(2);
+            Assert.AreEqual(Actual, expected);
+        }
+
+        [TestMethod]
+        public void GivenQuestionIdAndOptionId_WhenItIsValid_ThenReturnNextQuestionId()
+        {
+            OptionsRepository m = new OptionsRepository();
+            int Actual = 13;
+            int expected = m.datafetch(12, 1);
+            Assert.AreEqual(Actual, expected);
+        }
+
+        [TestMethod]
+        public void GivenQuestionIdAndOptionId_WhenItIsNotValid_ThenShouldNotReturnNextQuestionId()
+        {
+            OptionsRepository m = new OptionsRepository();
+            int Actual = 3;
+            int expected = m.datafetch(12, 1);
+            Assert.AreNotEqual(Actual, expected);
+        }
+
+
+        [TestMethod]
+        public void GivenQuestionIdAndOptionId_WhenItIsValid_ThenReturnCorrespondingMonitorId()
+        {
+            OptionsRepository m = new OptionsRepository();
+            int Actual = 19;
+            int expected = m.datafetch2(12, 5);
+            Assert.AreEqual(Actual, expected);
+        }
+
+        [TestMethod]
+        public void GivenQuestionIdAndOptionId_WhenItIsNotValid_ThenShouldNotReturnCorrespondingMonitorId()
+        {
+            OptionsRepository m = new OptionsRepository();
+            int Actual = 1;
+            int expected = m.datafetch2(12, 5);
+            Assert.AreNotEqual(Actual, expected);
+        }
+
+        [TestMethod]
+        public void GivenQuestionIdAndOptionId_WhenItIsValid_ThenReturnCorrespondingMonitorId2()
+        {
+            OptionsRepository m = new OptionsRepository();
+            int Actual = 10;
+            int expected = m.datafetch2(11, 3);
+            Assert.AreEqual(Actual, expected);
+        }
+
+        [TestMethod]
+        public void GivenQuestionIdAndOptionId_WhenItIsValid_ThenReturnCorrespondingNextQuestionId()
+        {
+            OptionsRepository m = new OptionsRepository();
+            int Actual = 13;
+            int expected = m.datafetch(12, 2);
+            Assert.AreNotEqual(Actual, expected);
+        }
+
+
+        [TestMethod]
+        public void GivenQuestionId_WhenItIsValid_ThenDisplayCorrectQuestion2()
+        {
+            QuestionRepository m = new QuestionRepository();
+            string Actual = "Select one of the required features from the following options.";
+            string expected = m.DataFetch(14);
+            Assert.AreEqual(Actual, expected);
+        }
+
+    }
+}
---
+++ PatientMonitorChatBotTest/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using chatbotrepo.Controllers;
+
+namespace PatientMonitorChatBotTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenQuestionId1_WhenItIsValid_ThenDisplayCorrectQuestion2()
+        {
+            ChatBotController m = new ChatBotController();
+            string Actual = "What is the basic requirement of your monitor?" + Environment.NewLine + " 1.Maternal and Fetal " + Environment.NewLine + " 2.Vital Signs" + Environment.NewLine + " 3.Bedside Patient Monitor" + Environment.NewLine + " 4.Central Monitoring System" + Environment.NewLine + " 5.IntelliVue(serves a wide range of care environment)";
+            string expected = m.Get();
+            Assert.AreNotEqual(expected, Actual);
+        }
+        [TestMethod]
+        public void Given_QuestionId_1_and_OptionId_1_Return_LinkId_2()
+        {
+            ChatBotController chatbot = new ChatBotController();
+            int expected = chatbot.GetLink(new WebApplication2.DAL.optiontable { question_id = 1, optionid = 1 });
+            int actual = 2;
+            Assert.AreEqual(expected,actual);
+
+        }
+
+        [TestMethod]
+        public void Given_LinkId_4_Return_Question_And_Options_with_Id_4()
+        {
+            ChatBotController chatbot = new ChatBotController();
+            string expected = chatbot.FetchQuestion(new WebApplication2.DAL.optiontable { linkid = 4 });
+            string actual = "Do you want video and audio interface?" + Environment.NewLine + "1.Yes" + Environment.NewLine + "2.No";
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Given_QId_And_OptId_fetch_MonitorName()
+        {
+            ChatBotController chatbot = new ChatBotController();
+            string expected = chatbot.MonitorFetch(new WebApplication2.DAL.optiontable { optionid = 3, question_id = 12 });
+            string actual = "Intellivue GS";
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
+
+    
+
